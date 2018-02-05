138a94234e9a04d1639b5a68bc11e5489e6f85b7
Jopno <35934376+Jopno@users.noreply.github.com>
2018-02-05 15:37:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            bool result = false;
+
+            do
+            {
+                Console.WriteLine("Give a number. ");
+                string line = Console.ReadLine();
+
+                int number;
+                result = int.TryParse(line, out number);
+                if (result)
+                {
+                    list.Add(number);
+                }
+            } while (result);
+
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                int number = list[i];
+                if (i == list.Count - 1)
+                    Console.WriteLine(number + "\n");
+                else
+                    Console.WriteLine(number + ",");
+            }
+
+
+            Console.WriteLine("Average: {0}", list.Average());
+            Console.WriteLine("Min: {0}", list.Min());
+            Console.WriteLine("Max: {0}", list.Max());
+            Console.WriteLine("Numbers: {0}", list.Count());
+
+            list.Sort();
+            list.ForEach(item => Console.Write(item + ","));
+
+        }
+    }
+}
