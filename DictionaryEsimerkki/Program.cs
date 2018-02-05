e01e209f2abfa2afe57cccacf8a4886015a052f7
Jopno <35934376+Jopno@users.noreply.github.com>
2018-02-05 16:51:33 +0200

    Add files via upload

---
+++ DictionaryEsimerkki/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stackStrings = new Stack<string>();
+            stackStrings.Push("1st");
+            stackStrings.Push("2nd");
+            stackStrings.Push("3rd");
+
+            Console.WriteLine(stackStrings.Peek());
+            Console.WriteLine("");
+
+            foreach (string s in stackStrings)
+            {
+                Console.WriteLine(s);
+            }
+
+            string temp = stackStrings.Pop();
+            Console.WriteLine("Just popped: {0}", temp);
+            Console.WriteLine("Stack count: {0}", stackStrings.Count);
+
+
+        }  
+
+    }
+}
