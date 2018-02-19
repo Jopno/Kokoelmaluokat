6253202adbe66fac859da55d556b83a9b5d8de16
Jopno <35934376+Jopno@users.noreply.github.com>
2018-02-19 10:51:03 +0200

    Add files via upload

---
+++ Task1 2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniAsioApplication
+{
+    class Program
+    {
+        static void ShowMenu(Dictionary<string, Student> students)
+        {
+            int number;
+
+            do
+            {
+                Console.WriteLine("Add a student 0");
+                Console.WriteLine("Remove a student 1");
+                Console.WriteLine("Search for a student 2");
+                Console.WriteLine("Quit 3");
+                bool result = int.TryParse(Console.ReadLine(), out number);
+                if (result)
+                {
+                    switch (number)
+                    {
+                        case 0:
+                            {
+                                AddStudent(students);
+                                break;
+                            }
+
+                        case 1:
+                            {
+                                RemoveStudent(students);
+                                break;
+                            }
+
+                        case 2:
+                            {
+                                SearchStudent(students);
+                                break;
+                            }
+                    }
+                }
+            } while (number != 3);
+        }
+
+        static void AddStudent(Dictionary<string, Student> students)
+        {
+            string firstName, lastName, asioid, group;
+            Console.WriteLine("Student first name: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Student last name: ");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Student Asio ID: ");
+            asioid = Console.ReadLine();
+            Console.WriteLine("Student group: ");
+            group = Console.ReadLine();
+
+            students.Add(asioid, new Student { FirstName = firstName, LastName = lastName, AsioID = asioid, Group = group });
+        }
+
+        static void RemoveStudent(Dictionary<string, Student> students)
+        {
+            string asioid;
+            Console.WriteLine("Write the AsioId of a student you want to remove: ");
+            asioid = Console.ReadLine();
+
+            if (students.ContainsKey(asioid))
+            {
+                students.Remove(asioid);
+                Console.WriteLine("Student with ID {0} removed", asioid);
+            }
+        }
+
+        static void SearchStudent(Dictionary<string, Student> students)
+        {
+            string asioid;
+            Student found;
+            Console.WriteLine("Write the AsioId of a student you want to search: ");
+            asioid = Console.ReadLine();
+
+            if (students.TryGetValue(asioid, out found))
+            {
+                Console.WriteLine("Student name: {0} {1}, AsioID: {2}, Group: {3}", found.FirstName, found.LastName, asioid, found.Group);
+            }
+
+            else
+            {
+                Console.WriteLine("No student with such ID");
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, Student> students = new Dictionary<string, Student>();
+
+            try
+            {
+                ShowMenu(students);
+            }
+
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Student with the same ID already exists");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ Task1 2/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniAsioApplication
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AsioID { get; set; }
+        public string Group { get; set; }
+    }
+}
